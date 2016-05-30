c10361c63e65f53a3fe70a492f26b1311c08756f
Ueslei de Souza <darkfrontcode@gmail.com>
2016-05-30 18:25:12 -0300

    Update home.js

--- server/angular/controllers/home.js
+++ server/angular/controllers/home.js
@@ -1,9 +1,22 @@
+import $ from 'jquery'
+
 export default class homeController {
     constructor(friends) {
         this.friends = friends
     }
 
+    random(){
+        return this.friends.random().first_name
+    }
+
     shuffle() {
-        this.name = this.friends.random().first_name
+        let random_friend = this.random()
+        let current_friend = $('#random_friend_name').text()
+
+        if(current_friend != random_friend){
+            this.name = random_friend
+        }else{
+            this.shuffle()
+        }
     }
 }
